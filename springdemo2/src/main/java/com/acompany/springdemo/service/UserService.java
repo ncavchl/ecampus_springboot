a259decef31bf3d3343a28434c484fb9d8326349
ncavchl <ncavchl@ajou.ac.kr>
2020-08-31 22:34:38 +0900

    집에서 a마저

--- springdemo2/src/main/java/com/acompany/springdemo/service/UserService.java
+++ springdemo2/src/main/java/com/acompany/springdemo/service/UserService.java
@@ -1,5 +1,16 @@
 package com.acompany.springdemo.service;
 
+import com.acompany.springdemo.model.User;
+
+import java.util.List;
+public interface UserService{
+    List<User> getUsers();
+
+    User getUserById(Integer userid);
+    User registUser(User user);
+    void modifyUser(Integer userid, User user);
+    void removeUser(Integer userid);
+}
 import com.acompany.springdemo.dao.UserRepository;
 import com.acompany.springdemo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,68 +20,68 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-@Service
-public class UserService {
-//    User users = new User(,);
-
-//    @Override
-//    public void createUser (Integer userid, String username){
-//        User user = new User(userid, username);
-//        this.users.add(user);
+//
+//@Service
+//public class UserService {
+////    User users = new User(,);
+//
+////    @Override
+////    public void createUser (Integer userid, String username){
+////        User user = new User(userid, username);
+////        this.users.add(user);
+////    }
+////
+////    @Override
+////    public void updateUser (Integer userid, String username){
+////        users.stream()
+////            .filter(x -> x.getUserid() == userid)
+////            .findAny()
+////            .orElseThrow(() -> new RuntimeException("Item not found"))
+////            .setUsername(username);
+////    }
+////
+////    @Override
+////    public void deleteUser(Integer userid){
+////        users.removeIf((User u) -> u.getUserid() == userid);
+////    }
+////
+////    @Override
+////    public User getUser(Integer userid){
+////        return user.stream()
+////                .filter(x -> x.getUserid() == userid)
+////                .findAny()
+////                .orElse(new User(0, "Not Available"));
+////    }
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    public Map<String, String> getMessage(){
+//        Map<String, String> res = new HashMap<>();
+//        res.put("greet", "Hello world");
+//
+//        return res;
 //    }
 //
-//    @Override
-//    public void updateUser (Integer userid, String username){
-//        users.stream()
-//            .filter(x -> x.getUserid() == userid)
-//            .findAny()
-//            .orElseThrow(() -> new RuntimeException("Item not found"))
-//            .setUsername(username);
+//    public List<User> getUsers(){
+//        System.out.println("서비스::");
+//        return userRepository.getAllusers();
 //    }
 //
-//    @Override
-//    public void deleteUser(Integer userid){
-//        users.removeIf((User u) -> u.getUserid() == userid);
+//    public User getUserByID(Integer userid){
+//        return userRepository.getUserByUserID(userid);
 //    }
 //
-//    @Override
-//    public User getUser(Integer userid){
-//        return user.stream()
-//                .filter(x -> x.getUserid() == userid)
-//                .findAny()
-//                .orElse(new User(0, "Not Available"));
+//    public User registUser(User user) {
+//        return userRepository.insertUser(user);
 //    }
-
-    @Autowired
-    private UserRepository userRepository;
-
-    public Map<String, String> getMessage(){
-        Map<String, String> res = new HashMap<>();
-        res.put("greet", "Hello world");
-
-        return res;
-    }
-
-    public List<User> getUsers(){
-        System.out.println("서비스::");
-        return userRepository.getAllusers();
-    }
-
-    public User getUserByID(Integer userid){
-        return userRepository.getUserByUserID(userid);
-    }
-
-    public User registUser(User user) {
-        return userRepository.insertUser(user);
-    }
-
-    public void modifyUser(Integer userid, User user){
-        user.setUserid(userid);
-        userRepository.updateUser(user);
-    }
-
-    public void removeUser(Integer userid){
-        userRepository.deleteUser(userid);
-    }
-}
+//
+//    public void modifyUser(Integer userid, User user){
+//        user.setUserid(userid);
+//        userRepository.updateUser(user);
+//    }
+//
+//    public void removeUser(Integer userid){
+//        userRepository.deleteUser(userid);
+//    }
+//}
